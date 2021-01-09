9d5108624f2f2c340b0fccc848e09e05c19ceb9c
Olya <spasitelevaoya@gmail.com>
2021-01-09 14:32:48 +0200

    j

---
+++ String/MainString.java
@@ -0,0 +1,25 @@
+package Pack1;
+
+/**
+ * Created by Home on 26.12.2020.
+ */
+public class MainString {
+    public static void main(String[] args)throws IndexOutOfBoundsException {{
+        StringPart1.showAZ();
+        System.out.println();
+        StringPart1.showza();
+        System.out.println();
+        StringPart1.showRus();
+        System.out.println();
+        StringPart1.showNumb();
+        System.out.println();
+        StringPart1.All();
+        System.out.println();
+        System.out.println(StringPart3.minWorld("Muuu hhhhh hhhh hhhhhhhhh hjj  hhhh"));
+        StringPart3.Howmanywords();
+        System.out.println();
+        System.out.println(StringPart3.DeleteWord("My,name.My is!My"));
+        System.out.println(StringPart3.reverse("My name is"));}}}
+
+
+
